123f4807ce6912143930bedf0bc953d73447a68c
Sania Hamid <hamid.sania@gmail.com>
2020-10-06 14:11:19 -0400

    printing the results

--- data/bigquery.py
+++ data/bigquery.py
@@ -11,26 +11,26 @@ Base = declarative_base()
 class PaymentTransaction(Base):
     """
     """
-    __tablename__ = 'payment_transaction_2019'
+    __tablename__ = 'sample_payments'
     msg_ref_id = Column(String, primary_key = True)
     cycle_date = Column(Date)
-    cycle_seq_num = Column(Integer)
+    #cycle_seq_num = Column(Integer)
     payment_tranche = Column(String)
     trans_time = Column(DateTime)
     acp_time = Column(DateTime)
     payment_amt = Column(Numeric)
     part_id_from = Column(String)
-    part_brch_cd_from = Column(String)
-    part_loc_cd_from = Column(String)
+    #part_brch_cd_from = Column(String)
+    #part_loc_cd_from = Column(String)
     part_id_to = Column(String)
-    part_brch_cd_to = Column(String)
-    part_loc_cd_to = Column(String)
-    trans_class = Column(String)
-    rjt_rsn_cd = Column(String)
-    mir_date = Column(Date)
-    mir_sess_nb = Column(Integer)
-    mir_seq_nb = Column(Integer)
-    status = Column(String)
+    #part_brch_cd_to = Column(String)
+    #part_loc_cd_to = Column(String)
+    #trans_class = Column(String)
+    #rjt_rsn_cd = Column(String)
+    #mir_date = Column(Date)
+    #mir_sess_nb = Column(Integer)
+    #mir_seq_nb = Column(Integer)
+    #status = Column(String)
     swift_msg_id = Column(String)
 
 class BigQueryHandler:
@@ -42,3 +42,10 @@ class BigQueryHandler:
         self.Session = sessionmaker()
         self.Session.configure(bind=self.engine)
         self.session = self.Session()
+        self.result = self.session.query(PaymentTransaction).all()
+        for row in self.result:
+            print("{}\t{}\t{}\t{}".format(row.msg_ref_id,row.cycle_date,row.part_id_from,row.part_id_to))
+
+
+if __name__ == "__main__":
+    test = BigQueryHandler('credentials.json')
