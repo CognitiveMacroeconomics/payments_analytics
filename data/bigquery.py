969f8fb6e03e84c5a7c8f6f1b7a5a4673d04c05f
Sania Hamid <hamid.sania@gmail.com>
2020-10-26 14:44:12 -0400

    writing to big query completed

--- data/bigquery.py
+++ data/bigquery.py
@@ -6,8 +6,8 @@ import pandas as pd
 from pandas.io import gbq
 import google.auth
 import os
-import matrix_parser
-import split_scale_parser
+import matrix_parser_new
+import split_scale_parser_new
 import numpy as np
 
 
@@ -21,6 +21,13 @@ class BigQueryHandler:
     def get_dataframe(self):
         return self.data_df
 
+def parse_chunk(chunk, parser_agg):
+
+    return pd.DataFrame(parser_agg.parse(chunk.to_dict("records"),\
+                        aggregation=True, aggregation_time=300),\
+                            columns=parser_agg.get_column_names())
+    
+
 
 def parser_write_subset(df, data_name, set_name, scalar, parser_ss, parser_agg,\
                         nr_chunks=1):
@@ -28,24 +35,48 @@ def parser_write_subset(df, data_name, set_name, scalar, parser_ss, parser_agg,\
     print("Starting with processing: {}".format(set_name))
     
     if data_name == "":
-        destination_table="deeplearnig.sample_{}".format(set_name)
+        table_name = "deeplearnig.sample_{}".format(set_name)
     else:
-        destination_table="deeplearnig.sample_{}_{}".format(set_name, data_name)
+        table_name = "deeplearnig.sample_{}_{}".format(set_name, data_name)
     
-    print(destination_table)
+    print(table_name)
     counter = 0
+    
     print("length of the dataframe is:{}".format(len(df)))
     print("Number of chunks are:{}".format(nr_chunks))
     print("Step size is:{}".format(len(df)/nr_chunks))
+    
     step_size = int(np.ceil(len(df)/nr_chunks))
+    
     print("Step size is:{}".format(step_size))
+    
     for step in range(0,len(df), step_size):
+        
         print("Step is:{}".format(step))
+        
         if step+step_size<=len(df):
-            print(step+step_size)
-            
+            chunk = df.iloc[list(range(step,step+step_size))]
+            print("Length of the chunk size is:{}".format(len(chunk)))
+        else:
+            chunk = df.iloc[step:len(df)]
+            print("Here")
+            print("Length of the chunk size is:{}".format(len(chunk)))
 
-    return None
+        print("Scaling iterations:{}\n".format(counter))
+        
+
+        if set_name != "test":
+            chunk = parse_chunk(chunk, parser_agg)
+            parser_ss.scale_all(chunk, scaler)
+        
+        chunk.to_gbq(destination_table=table_name,\
+                project_id="deeplearning-291017",\
+                if_exists='append')
+            
+        print("/r{:0.4f}".format((counter*100/nr_chunks)), end="")
+    
+    print("\rFinished parsing ans writing: {}".format(set_name), end="")
+            
 
 
 if __name__ == "__main__":
@@ -54,7 +85,7 @@ if __name__ == "__main__":
 
     query = "SELECT acp_time, payment_amt, part_id_from, part_id_to,\
             swift_msg_id FROM\
-            deeplearning-291017.deeplearnig.sample_payments"
+            deeplearning-291017.deeplearnig.sample_payments order by acp_time"
 
     prj_id = "deeplearning-291017"
     bqh = BigQueryHandler(query, prj_id)
@@ -66,7 +97,8 @@ if __name__ == "__main__":
                         'part_id_from': 'sender_bank',\
                         'part_id_to': 'receiver_bank',\
                         'swift_msg_id': 'payment_type'}, inplace=True)
-    print(df.head())
+    print(df.tail())
+    #print(df.iloc[95:len(df)])
 
     #bank_list = ['ATBRCA','BCANCA','BLCMCA','BNDCCA','BNPACA','BOFACA',\
     #             'BOFMCA','CCDQCA','CIBCCA','CUCXCA','HKBCCA','ICICCA',\
@@ -80,7 +112,7 @@ if __name__ == "__main__":
     #print(df.head())
 
     ############################################################################
-    # parser = matrix_parser.MatrixParser(bank_list=bank_list)
+    # parser = matrix_parser_new.MatrixParser(bank_list=bank_list)
     
 
     # output_array = parser.parse(df.to_dict("records"), aggregation=True,\
@@ -93,8 +125,8 @@ if __name__ == "__main__":
     ############################################################################
 
     ############################################################################
-    # parser_agg = matrix_parser.MatrixParser(bank_list=bank_list)
-    # parser_ss = split_scale_parser.SplitScaleParser()
+    # parser_agg = matrix_parser_new.MatrixParser(bank_list=bank_list)
+    # parser_ss = split_scale_parser_new.SplitScaleParser()
 
     # train_ids, val_ids, test_ids = parser_ss.split_train_val_test_index(df)
     # scaler_am = parser_ss.make_amount_scaler(df, train_ids)
@@ -115,8 +147,8 @@ if __name__ == "__main__":
     VAL_SIZE = 0.5
     NR_CHUNKS = 2
     
-    parser_agg = matrix_parser.MatrixParser(bank_list=bank_list)
-    parser_ss =split_scale_parser. SplitScaleParser(\
+    parser_agg = matrix_parser_new.MatrixParser(bank_list=bank_list)
+    parser_ss =split_scale_parser_new. SplitScaleParser(\
                                     val_test_size=VAL_TEST_SIZE,\
                                     val_size=VAL_SIZE)
 
@@ -125,16 +157,16 @@ if __name__ == "__main__":
 
     #print(df.iloc[train_ids])
 
-    parser_write_subset(df.iloc[train_ids],"_1y","train", scaler, parser_ss, parser_agg, nr_chunks = NR_CHUNKS)
+    parser_write_subset(df.iloc[train_ids],"_1y","train", scaler, parser_ss,\
+                        parser_agg, nr_chunks = NR_CHUNKS)
     
-    #def parser_write_subset(df.iloc[train_ids], "_1y", "validate", scaler,\
-    #                        parser_ss, parser_agg, nr_chunks = NR_CHUNKS)
+    parser_write_subset(df.iloc[val_ids], "_1y", "validate", scaler,\
+                           parser_ss, parser_agg, nr_chunks = NR_CHUNKS)
 
-    #def parser_write_subset(df.iloc[train_ids], "_1y", "test", scaler,\
-    #                        parser_ss, parser_agg, nr_chunks = NR_CHUNKS)
+    parser_write_subset(df.iloc[test_ids], "_1y", "test", scaler,\
+                           parser_ss, parser_agg, nr_chunks = NR_CHUNKS)
 
 
-    
     ############################################################################
     
 
