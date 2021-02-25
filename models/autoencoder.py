c3a383e3c2a0134ea3602a6313a0c6575112ae74
ellenvanderwoerd <40057861+ellenvanderwoerd@users.noreply.github.com>
2021-02-25 10:34:06 +0100

    Update autoencoder.py

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -1,11 +1,10 @@
-from keras.models import Model
-from keras.layers import Input, GRU, Dense, Dropout, RepeatVector, SimpleRNN
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Input, GRU, Dense, Dropout, RepeatVector, SimpleRNN
 
 #input_shape (window_size, feature_size)
-def make_autoencoder(input_shape, activation = "relu", hidden_layer_size = 50):
+def make_autoencoder(input_shape, activation = "relu", hidden_layer_size = 16):
     """
     make the autoencoder model with a specific input_shape.
-
     Parameters:
     input_shape (int,int): (1,number of features)
     activation (str): activation function. default is relu
@@ -13,17 +12,9 @@ def make_autoencoder(input_shape, activation = "relu", hidden_layer_size = 50):
         
     Model specifics:
     """
-    
+
     inp_layer = Input(shape=input_shape)
     hidden_layer = Dense(hidden_layer_size, activation = activation)(inp_layer)
-    output_layer = Dense(7, activation = activation)(hidden_layer_size)
+    output_layer = Dense(input_shape[1], activation = activation)(hidden_layer)
     model = Model(inputs=inp_layer, outputs=output_layer, name="simple_autoencoder")
     return model
-
-
-
-
-
-if __name__=="__main__":
-    model = make_rnn((1,7))
-    print(model.summary())
