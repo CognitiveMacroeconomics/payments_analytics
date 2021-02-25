54aaa3d57c1eb48f146e718a6cc39b5e5aab1410
ellenvanderwoerd <40057861+ellenvanderwoerd@users.noreply.github.com>
2021-02-25 11:17:25 +0100

    Update train_lstm.py

--- train_lstm.py
+++ train_lstm.py
@@ -1,58 +1,40 @@
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from dataiku import pandasutils as pdu
-from target2_analytics.data.dataiku_parser import IkuParser
 from target2_analytics.models.lstm import make_lstm
-from tensorflow.keras.models import load_model
-from tensorflow.keras import Model
-from sklearn.manifold import TSNE
-from sklearn.decomposition import PCA
 from target2_analytics.data.windows import MemoryPreparer
+from tensorflow.keras.losses import MSE
 
-#global params
-WINDOW_SIZE = 10
+# Global parameters (adapt this to your needs)
+WINDOW_SIZE = 50
 HIDDEN_LAYER_SIZE = 16
 EPOCHS = 100
-BATCH_SIZE = 100
+BATCH_SIZE = 2128
 
-
-# Read recipe inputs
-t2_parsed_train = dataiku.Dataset("t2_anon_sql_train_1y")
+# Read recipe inputs: adapt to own input from matrix parser, shoudl be pandas dataframe
+t2_parsed_train = dataiku.Dataset("t2_parsed_train")
 t2_parsed_train_df = t2_parsed_train.get_dataframe()
-t2_parsed_validate = dataiku.Dataset("t2_anon_sql_validate_1y")
+t2_parsed_validate = dataiku.Dataset("t2_parsed_validate")
 t2_parsed_validate_df = t2_parsed_validate.get_dataframe()
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-mp = MemoryPreparer(window_size = WINDOW_SIZE)
-t2_windowed_val_array = mp.prepare(t2_parsed_validate_df)
-t2_windowed_train_array = mp.prepare(t2_parsed_train)
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-#Make model
-inp_shape = t2_parsed_train_df.iloc[0].shape
+# Prepare windowed array
+mp_train = MemoryPreparer(window = True, window_size = WINDOW_SIZE, batch_size=BATCH_SIZE)
+t2_windowed_train_gen = mp_train.prepare(t2_parsed_train_df)
+mp_val = MemoryPreparer(window = True, window_size = WINDOW_SIZE, batch_size=BATCH_SIZE)
+t2_windowed_validate_gen = mp_val.prepare(t2_parsed_validate_df)
+
+#Make model 
+inp_shape = (1, t2_windowed_train_array.shape[1])
 model = make_lstm(input_shape=inp_shape, hidden_layer_size=HIDDEN_LAYER_SIZE)
 model.compile(optimizer="adam", loss="mse")
-print(model.summary())
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 #Fit model
-history = model.fit(t2_windowed_train_array, t2_windowed_train_array ,
-             validation_data=(t2_windowed_train_array, t2_windowed_train_array),
-             epochs=EPOCHS,
-             batch_size=BATCH_SIZE)
-# NB: DSS supports several kinds of APIs for reading and writin+g data. Please see doc.
+history = model.fit(t2_windowed_train_gen, epochs=EPOCHS)
+## To canada: somehow we lost the validate set here. I am not sure why. We should ask Timothy.
 
 hist_df = pd.DataFrame(history.history)
 
-# Write recipe outputs
-model_dir = dataiku.Folder("1y_lstm").get_info()["path"]
-hist_df.to_csv("{}/{}".format(model_dir, "lstm_results.csv"))
+# Write recipe outputs: adapt to own system
+model_dir = dataiku.Folder("lstm_folder").get_info()["path"]
+hist_df.to_csv("{}/{}".format(model_dir, "lstm_results.csv"), mode="a", header=False)
 model.save("{}/{}".format(model_dir, "lstm"))
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-fig = hist_df.plot().get_figure()
-fig.savefig("{}/{}".format(model_dir, "lstm.jpg"))
