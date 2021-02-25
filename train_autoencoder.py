6cef9280ee633fdc00c7d1bf899d9ef9fc3f5da9
ellenvanderwoerd <40057861+ellenvanderwoerd@users.noreply.github.com>
2021-02-25 11:19:59 +0100

    Change to code with generator

--- train_autoencoder.py
+++ train_autoencoder.py
@@ -4,9 +4,11 @@ from dataiku import pandasutils as pdu
 from target2_analytics.data.windows import MemoryPreparer
 from target2_analytics.models.autoencoder import make_autoencoder
 
-#global params
-WINDOW_SIZE = 10
+#global params (adapt to what you need, start with low window size!)
+WINDOW_SIZE = 50
 HIDDEN_LAYER_SIZE = 16
+EPOCHS = 100
+BATCH_SIZE = 2128
 
 # Read recipe inputs CANADA should adapt this to own system, just load the train and validate
 # as a pandas dataframe, which you prepared before.
@@ -16,9 +18,10 @@ t2_parsed_validate = dataiku.Dataset("t2_parsed_validate")
 t2_parsed_validate_df = t2_parsed_validate.get_dataframe()
 
 #Prepare matrix
-mp = MemoryPreparer(window = False)
-t2_val_array = mp.prepare(t2_parsed_validate_df)
-t2_train_array = mp.prepare(t2_parsed_train_df)
+mp_train = MemoryPreparer(window = False, batch_size=BATCH_SIZE)
+t2_val_gen = mp_train.prepare(t2_parsed_validate_df)
+mp_val = MemoryPreparer(window = False, batch_size=BATCH_SIZE)
+t2_train_gen = mp_val.prepare(t2_parsed_train_df)
 
 #Make model 
 inp_shape = (1, t2_train_array.shape[1])
@@ -26,10 +29,7 @@ model = make_autoencoder(input_shape=inp_shape, hidden_layer_size=HIDDEN_LAYER_S
 model.compile(optimizer="adam", loss="mse")
 
 #Fit model
-history = model.fit(t2_train_array, t2_train_array,
-             validation_data=(t2_val_array, t2_val_array),
-             epochs=2,
-             batch_size=100)
+history = model.fit(t2_train_gen, epochs=EPOCHS)
 
 hist_df = pd.DataFrame(history.history)
 
